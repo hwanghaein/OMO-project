67a2ad75056666c15f51db834d2a90b8803401f9
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-07-19 15:06:53 +0900

    feat first commit
    This is first commit

---
+++ src/main/java/com/OmObe/OmO/OmOApplication.java
@@ -0,0 +1,13 @@
+package com.OmObe.OmO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmOApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OmObe/OmO/OmOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OmObe.OmO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
